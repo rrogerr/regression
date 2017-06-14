c0a835aa4301b234c83c0d54fed89781a09b0ef2
rrogerr <rgcbll@gmail.com>
2017-06-14 12:19:48 -0500

    eso es bola

---
+++ intro_least_squares.R
@@ -0,0 +1,21 @@
+# pairs father height-son height, each point is 
+# weighted by the number of occurrences of that
+# combination in the data
+
+library(UsingR)
+library(ggplot2)
+
+data(galton)
+
+library(reshape2)
+library(dplyr)
+freqData <- as.data.frame(table(galton$child, galton$parent))
+names(freqData) <- c("child", "parent", "freq")
+freqData$child <- as.numeric(as.character(freqData$child))
+freqData$parent <- as.numeric(as.character(freqData$parent))
+g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+g <- g  + scale_size(range = c(2, 20), guide = "none" )
+# g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+g <- g + geom_point(aes(colour=freq, size = freq))
+g <- g + scale_colour_gradient(low = "lightblue", high="white")                    
+g
