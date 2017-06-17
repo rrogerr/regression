db6e1cfdae4c9d6c77cca3bf4308f73d08724e3c
rrogerr <rgcbll@gmail.com>
2017-06-16 19:26:37 -0500

    o rly

---
+++ residuals.R
@@ -0,0 +1,57 @@
+#influence measures, dfbetas, hatvalues
+
+x <- rnorm(20)
+y <- x + rnorm(20, sd = 0.1)
+
+# aligned outlier
+
+x0 <- 5
+y0 <- 5
+
+x1 <- c(x0, x)
+y1 <- c(y0, y)
+
+plot(x1, y1)
+
+fit <- lm(y1~x1)
+round(hatvalues(fit)[1:10],3)
+round(dfbetas(fit)[1:10,2],3)
+
+# non-aligned outlier
+
+x0 <- 5
+y0 <- 20
+
+x1 <- c(x0, x)
+y1 <- c(y0, y)
+
+plot(x1, y1)
+
+fit <- lm(y1~x1)
+round(hatvalues(fit)[1:10],3)
+round(dfbetas(fit)[1:10,2],3)
+
+x0 <- 5
+y0 <- 0
+
+x1 <- c(x0, x)
+y1 <- c(y0, y)
+
+plot(x1, y1)
+
+fit <- lm(y1~x1)
+round(hatvalues(fit)[1:10],3)
+round(dfbetas(fit)[1:10,2],3)
+
+x0 <- 0
+y0 <- 5
+
+x1 <- c(x0, x)
+y1 <- c(y0, y)
+
+plot(x1, y1)
+
+fit <- lm(y1~x1)
+round(hatvalues(fit)[1:10],3)
+round(dfbetas(fit)[1:10,2],3)
+# oh really meme
